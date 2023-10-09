40f7d5bf67d925e8fda3c2d23044d8946c6f44c4
exPANDAbl3 <dipeshpadelkar@gmail.com>
2023-10-09 15:16:37 +0530

    Removed Unnecessary Test Cases

--- cypress/integration/examples/Test10.js
+++ cypress/integration/examples/Test10.js
@@ -1,27 +1,22 @@
 /// <reference types="Cypress" />
+import 'cypress-iframe'
 
-describe("Find Elements with Specific Color", () => {
-  it("Should find elements with color #3498db", () => {
-    // Visit the website or page you want to test
-    cy.visit("https://dev.sothebysmotorsport.com/"); // Replace with the actual website URL
+describe('Frame', function()
+{
+    before(function(){ 
 
-    // Define the expected color in RGB format
-    const expectedColor = "rgb(0, 180, 229)"; // RGB for #3498db
+        cy.fixture('example')
+    })
 
-    // Use Cypress to find all elements on the page
-    cy.get("button").each(($button) => {
-      // Get the computed color of each element
-      cy.wrap($button)
-        .should("have.css", "background-color")
-        .then((color) => {
-          // Check if the computed color matches the expected color
-          if (color === expectedColor) {
-            // Log or perform actions on the elements with the expected color
-            cy.log(
-              `Found element with color #3498db: ${$element.prop("tagName")}`
-            );
-          }
-        });
-    });
-  });
-});
+
+
+
+it('Frame Test', function()
+{
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+   
+    cy.getMaxListeners('input[name="name"]:nth-child(2)').type("Bob")
+    cy.get('select').select("Female")
+    
+})
+})
